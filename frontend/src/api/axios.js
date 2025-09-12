09aa1f165b5641e95a87a49fa57f7172a0a21f0a
tariq3101 <khantariq10648@gmail.com>
2025-09-12 22:18:28 +0530

    Remove api folder from repo

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/auth", // match your backend PORT
-  withCredentials: true, // send cookies
-});
-
-export default api;
